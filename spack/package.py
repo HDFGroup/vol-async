9de6ea73b038be9068d48229761e00583a6c909e
Houjun Tang <htang4@lbl.gov>
2021-08-09 16:23:18 -0700

    Add cmake files and spack recipe

---
+++ spack/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+class Hdf5VolAsync(CMakePackage):
+    """An asynchronous I/O framework that provides support for all I/O operations
+       and manages data dependencies transparently and automatically."""
+
+    homepage = "https://hdf5-vol-async.readthedocs.io"
+    git      = "https://github.com/hpc-io/vol-async"
+
+    maintainers = ['houjun', 'sbyna']
+
+    version('develop', branch='develop')
+
+    depends_on('argobots@main')
+    depends_on('hdf5@develop-1.13+mpi+threadsafe')
+
+    def cmake_args(self):
+        """Populate cmake arguments for HDF5 VOL."""
+        spec = self.spec
+
+        args = [
+            '-DBUILD_SHARED_LIBS:BOOL=ON',
+            '-DBUILD_TESTING:BOOL=ON',
+        ]
+        return args
+
+#    def check_build(self):
+#        """Run ctest after building project."""
+#        with working_dir("test"):
+#            ctest(parallel=False)
